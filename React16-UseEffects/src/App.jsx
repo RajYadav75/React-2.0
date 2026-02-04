e05bab5df055bc358eae79978964ea0cff147d4a
RajYadav75 <rajyadav751811@gmail.com>
2026-02-04 07:05:52 +0530

    Use Effect Hook

---
+++ React16-UseEffects/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+//  The useEffect hook in React lets you run code automatically
+// when somthing changes or when a component loads
+
+// It's like setting a task to happen after the screen update
+//  or when certain data is ready
+
+// SYNTAX: 
+// useEffect(() => {
+  //  Code to run
+// },[dependency]);
+
+// useEffect(() => {}); Run on every render
+// useEffect(() => {},[]); Run only once after the initial render
+// useEffect(() => {},[count]); Run on initial render and when count changes
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
