d6325948d26577d37ac28cebb962609ef5135839
RajYadav75 <rajyadav751811@gmail.com>
2026-02-04 07:48:36 +0530

    Digital CLock

---
+++ React18-DigitalClock/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [time, setTime] = useState(new Date());
+
+
+  return (
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  )
+}
+
+export default App
