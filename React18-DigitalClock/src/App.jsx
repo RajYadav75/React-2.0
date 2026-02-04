1282822758349e4376348a127db382f848cf46e2
RajYadav75 <rajyadav751811@gmail.com>
2026-02-04 08:02:12 +0530

    Digital Clock

--- React18-DigitalClock/src/App.jsx
+++ React18-DigitalClock/src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
-function App() {
+function App()
+{
   const [time, setTime] = useState(new Date());
 
+  useEffect(() =>
+  {
+    const timerId = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(timerId);
+  }, []);
 
+  const formattedTime = time.toLocaleTimeString('en-US',
+    { hour: '2-digit', minute: '2-digit', second: '2-digit' }
+  )
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className='clock-container'>
+      <div className="clock">
+        {formattedTime}
+      </div>
+
     </div>
   )
 }
