726aaab4bb733e592d7283bf596495a2f8efce52
RajYadav75 <rajyadav751811@gmail.com>
2026-02-04 23:09:01 +0530

    UseRef Hook

---
+++ React19-UseRefHookInReact/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// useRef 
+// - persist vales across renders
+// - Does not cause the component to re-render when the value changes
+function App() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
