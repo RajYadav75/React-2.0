cee83a07ed2f05cab10a285ed7efc5524d27b61c
RajYadav75 <rajyadav751811@gmail.com>
2026-02-05 09:20:44 +0530

    Use Ref

--- React19-UseRefHookInReact/src/App.jsx
+++ React19-UseRefHookInReact/src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useRef, useState } from 'react';
 // useRef 
 // - persist vales across renders
 // - Does not cause the component to re-render when the value changes
 function App() {
+  // const myRef = useRef('Hello World!');
+  // {
+  //   current:"Hello World!"
+  // }
+  // console.log(myRef);
+
+  const [stateCount,setStateCount] = useState(0);
+  const refCount = useRef(0);
+  useEffect(()=>{
+    console.log(`Component Re-Render`);
+    
+  },[])
+
+  const incrementStateCount = () => {
+    setStateCount(stateCount + 1);
+  }
+
+  const incrementRefCount = () => {
+    refCount.current += 1;
+    console.log(`Ref Count : ${refCount.current}`);
+  }
   return (
     <div>
-
+      {/* <h1>Learn React {myRef.current}</h1> */}
+      <p>State Count : {stateCount}</p>
+      <button onClick={incrementStateCount}>Increment State Count</button><br/>
+      <p>Ref Count : {refCount.current}</p>
+      <button onClick={incrementRefCount}>Increment Ref Count</button>
     </div>
   )
 }
