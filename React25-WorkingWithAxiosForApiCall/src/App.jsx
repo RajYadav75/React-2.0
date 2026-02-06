d33b6aa5ed0a124185de8c4754f9d5d6b1d1e506
RajYadav75 <rajyadav751811@gmail.com>
2026-02-06 09:47:42 +0530

    Working WIth Axios

---
+++ React25-WorkingWithAxiosForApiCall/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import './App.css';
+import axios from 'axios'
+function App()
+{
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error,setError] = useState(null);
+
+  useEffect(() =>
+  {
+    setLoading(true);
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) =>
+      {
+        console.log(response);
+        setData(response.data);
+        setLoading(false);
+        throw new Error('Something went wrong');
+      })
+      .catch((error)=>{
+        console.error(`Error Fetching Data `,error);
+        setError('Failed to fetch the data');
+        setLoading(false);
+      })
+  }, []);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+  return (
+    <div>
+      <h1>API's</h1>
+      <ul>
+        {data.map((post) => (
+          <dl key={post.id}>
+            <dt> <strong>{post.id}. {post.title}</strong></dt>
+            <dd>{post.body}</dd>
+          </dl>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
