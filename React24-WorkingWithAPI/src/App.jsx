73f80c71fa8c73921d858e24283ed4f03e4ea25b
RajYadav75 <rajyadav751811@gmail.com>
2026-02-06 09:34:07 +0530

    Handling Loading And Error

--- React24-WorkingWithAPI/src/App.jsx
+++ React24-WorkingWithAPI/src/App.jsx
@@ -1,23 +1,44 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
+function App()
+{
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error,setError] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() =>
+  {
+    setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(json => setData(json));
-  },[]);
+      .then(json =>
+      {
+        setData(json);
+        setLoading(false);
+        throw new Error('Something went wrong');
+      })
+      .catch((error)=>{
+        console.error(`Error Fetching Data `,error);
+        setError('Failed to fetch the data');
+        setLoading(false);
+      })
+  }, []);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
   return (
     <div>
       <h1>API's</h1>
       <ul>
-        {data.map((post)=>(
+        {data.map((post) => (
           <dl key={post.id}>
-              <dt> <strong>{post.id}. {post.title}</strong></dt>
-              <dd>{post.body}</dd>
-            </dl>
+            <dt> <strong>{post.id}. {post.title}</strong></dt>
+            <dd>{post.body}</dd>
+          </dl>
         ))}
       </ul>
     </div>
