4a3db91edcd16cf8217b24af4422e6181fc149cd
RajYadav75 <rajyadav751811@gmail.com>
2026-02-07 10:00:00 +0530

    watch the input value

--- react30-reacthookform/src/App.jsx
+++ react30-reacthookform/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 import {useForm} from "react-hook-form";
+import {useEffect} from "react";
 
 function App() {
-  const {register,handleSubmit} = useForm(0)
-    const onSubmit = (data) => {
-        console.log(data)
-    }
+  const {register,handleSubmit,watch} = useForm(0)
+    const onSubmit = (data) => console.log(data)
+
+    const watchedName = watch('name');
+  const watchedEmail = watch('email');
+
+  useEffect(() => {
+      console.log('Watched Name:', watchedName);
+  },[watchedName]);
+  useEffect(() => {
+      console.log('Watched Email:', watchedEmail);
+  },[watchedEmail]);
   return (
     <div>
       <h1>Forms in React</h1>
